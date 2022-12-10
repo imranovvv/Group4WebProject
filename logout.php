ef685a856801db458d6783667e2bb64bcd8fbda3
imranovvv <imranhafizuddin@gmail.com>
2022-12-10 16:59:19 +0800

    logout to index.html

--- logout.php
+++ logout.php
@@ -4,7 +4,7 @@
 	{
 		session_destroy( );
 		echo "You have successfully logged out.";
-		header("Location: index.php");
+		header("Location: index.html");
 	}
 	else
 		echo " No session exists or session is expired. Please log in again";
