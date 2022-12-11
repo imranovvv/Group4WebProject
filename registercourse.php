53a23468801dd067b2f68f3650f80ac0fb089be6
imranovvv <imranhafizuddin@gmail.com>
2022-12-11 22:17:25 +0800

    move to user.php after delay

--- registercourse.php
+++ registercourse.php
@@ -2,10 +2,9 @@
 	session_start();
 	$con=mysqli_connect("localhost", "root", "","login_db") or die("Cannot connect to server");
 	$courseid=@$_GET['id'];
-	$message="This course is already full";
-	$message2="Successfully registered!";
+	$username=$_SESSION['userid'];
 
-	$sql="INSERT INTO registercourse VALUES('{$_SESSION['userid']}','$courseid')";
+	$sql="INSERT INTO registercourse VALUES('$username','$courseid')";
 
 	$sql_restrict="SELECT quota FROM course WHERE courseid='$courseid'";
 	$result_restrict=mysqli_query($con,$sql_restrict);	
@@ -14,11 +13,22 @@
 	$sql_count="SELECT courseid FROM registercourse WHERE courseid='$courseid'";
 	$result_count=mysqli_query($con,$sql_count);
 	$no_of_rows=mysqli_num_rows($result_count);
+	$row2=mysqli_fetch_array($result_count);
 
+	$sql_duplicate="SELECT * FROM registercourse WHERE username = '$username' AND courseId ='$courseid'";
+	$result_duplicate=mysqli_query($con,$sql_duplicate);
+	if(mysqli_num_rows($result_duplicate)>0){
+		 $message = "You have already registered for this course";
+                  	echo "<script type='text/javascript'>alert('$message');</script>";
+                  	echo "<script>setTimeout(function () { window.location.href = 'user.php'; }, 1000);</script>";
+
+	}
 	if($no_of_rows>=$row[0])
 	{
 		   $message = "This course is already full";
-                  	echo "<script type='text/javascript'>alert('$message');</script>";
+           echo "<script type='text/javascript'>alert('$message');</script>";
+           echo "<script>setTimeout(function () { window.location.href = 'user.php'; }, 1000);</script>";
+
 	}
 	else
 	{
@@ -26,7 +36,8 @@
 		{
 		    $message = "Successfully saved";
                   	echo "<script type='text/javascript'>alert('$message');</script>";
-			header("location:user.php");
+                  	echo "<script>setTimeout(function () { window.location.href = 'user.php'; }, 1000);</script>";
+
 		}
 		else
 		{
